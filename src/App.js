961aae9b644abe97b57033bc0ca5c09d223d4dc4
Nikhil Gautam <nikhilnn@blah.com>
2020-07-28 14:36:51 +0530

    created mvp for movie app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import Header from './components/header';
+import Search from './components/search';
+import Movie from './components/movie';
 
-function App() {
+const MOVIE_API_URL = "http://www.omdbapi.com/?s=man&apikey=4a3b711b"
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMsg, setErrorMsg] = useState("");
+ 
+  const getAllMovies = () =>{
+    fetch(MOVIE_API_URL).then(response => response.json()).then(jsonResp =>{
+      setMovies(jsonResp.Search);
+      console.log(movies,"sf")
+      setLoading(false);
+    })
+  }
+  useEffect(() => {
+    getAllMovies();
+  }, [])
+
+  const search = (searchValue) => {
+    setLoading(true);
+    setErrorMsg(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`).then(resp => resp.json()).then(jsonResp => {
+      if (jsonResp.Response === "True") {
+        setMovies(jsonResp.Search);
+        setLoading(false);
+      } else {
+        setErrorMsg(jsonResp.Error);
+        setLoading(false);
+        getAllMovies();
+      }
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div>
-        Welcome to React Boilerplate
-      </div>
+        <Header title="Moviex"/>
+        <Search search={search}/>
+        <p className="App-intro">Hottest picks for you !!</p>
+            {errorMsg && <div className="errorMsg">{errorMsg}</div>}
+        <div className="movies">
+          {loading ? (
+            <span>loading...</span>
+          ):(
+            movies.map((movie, index)=>(
+              <Movie key={`${index}-${movie.Title}`} movie={movie} />
+            ))
+          )}
+        </div>
     </div>
   );
 }
